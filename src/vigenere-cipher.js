99e423246ae6038c573461b7cf15c2650e072090
mldx <anton.zaprutski@gmail.com>
2022-04-28 19:07:40 +0300

    feat: add Vigenere cipher function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  arrayL = [
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z'
+  ]
+
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    try {
+      let len = message.length;
+      key = key.padEnd(len, key).toUpperCase().split('');
+      message = message.toUpperCase().split('');
+      message.forEach((value, index) => {
+        if (this.arrayL.indexOf(value) === -1) {
+          key.splice(index, 0, ' ');
+        }
+      });
+      key = key.splice(0, len);
+      message = message.map((value, index) => {
+        if (this.arrayL.indexOf(value) !== -1) {
+          let sumIndex = this.arrayL.indexOf(value) + this.arrayL.indexOf(key[index]);
+          return this.arrayL[sumIndex > 25 ? sumIndex - 26 : sumIndex]
+        } else {
+          return value;
+        }
+      })
+      return this.direct === true ? message.join('') : message.reverse().join('')
+    } catch {
+      throw new Error('Incorrect arguments!')
+    }
+  }
+
+  decrypt(message, key) {
+    try {
+      let len = message.length;
+      key = key.padEnd(len, key).toUpperCase().split('');
+      message = message.toUpperCase().split('');
+      message.forEach((value, index) => {
+        if (this.arrayL.indexOf(value) === -1) {
+          key.splice(index, 0, ' ');
+        }
+      });
+      key = key.splice(0, len);
+      message = message.map((value, index) => {
+        if (this.arrayL.indexOf(value) !== -1) {
+          let sumIndex = this.arrayL.indexOf(value) - this.arrayL.indexOf(key[index]);
+          return this.arrayL[sumIndex < 0 ? this.arrayL.indexOf(value) + 26 - this.arrayL.indexOf(key[index]) : this.arrayL.indexOf(value) - this.arrayL.indexOf(key[index])];
+        } else {
+          return value;
+        }
+      })
+      return this.direct === true ? message.join('') : message.reverse().join('')
+    } catch {
+      throw new Error('Incorrect arguments!')
+    }
   }
 }
 
